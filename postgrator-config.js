df808c253ce4c1bd442806f24cec8e9e235e7be9
Matt Vegh <mdv240@gmail.com>
2019-10-16 21:21:22 -0500

    first

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,9 +5,9 @@ module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DB_URL
-    : process.env.DB_URL,
-    "database": process.env.MIGRATION_DB_NAME,
-    "username": process.env.MIGRATION_DB_USER,
-    "password": process.env.MIGRATION_DB_PASS
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+    "database": process.env.MIGRATION_DATABASE_NAME,
+    "username": process.env.MIGRATION_DATABASE_USER,
+    "password": process.env.MIGRATION_DATAASEB_PASS
   }
