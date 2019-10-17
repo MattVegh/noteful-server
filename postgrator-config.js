2e6a078bcfb44ef2636ff3f3da5502743c082891
Matt Vegh <mdv240@gmail.com>
2019-10-16 21:36:35 -0500

    heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,6 +7,7 @@ module.exports = {
     "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
+    "ssl": !!process.env.SSL,
     "database": process.env.MIGRATION_DATABASE_NAME,
     "username": process.env.MIGRATION_DATABASE_USER,
     "password": process.env.MIGRATION_DATAASEB_PASS
